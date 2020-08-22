32e7fad28fd5ab51f224d2cbc01bd78fbad03b57
an2050 <zaem087@gmail.com>
2020-08-22 19:59:37 +0300

    remove cached RV files

--- RV/Python/rv.py
+++
@@ -1,3 +0,0 @@
-# from rv import commands; from pymu import MuSymbol; mkFr = commands.markedFrames(); print(mkFr); MuSymbol('commands.play')();MuSymbol('export_utils.exportMarkedFrames')('d:/Projects/knp/test.####.exr')
-
-from rv import commands; commands.addSource('d:/Projects/knp/FAU/plane_damage/out/cam_02/v01/cam_02_v01.####.exr')
